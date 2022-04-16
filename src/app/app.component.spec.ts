6110c42c7608f904903ab8ccc8131a82de9f9f1b
Umar Uzman Faraaz <umaruzman.lk@gmail.com>
2022-04-17 00:34:22 +0530

    Project Renamed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rms-frontend'`, () => {
+  it(`should have as title 'angular-antd-skeleton'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rms-frontend');
+    expect(app.title).toEqual('angular-antd-skeleton');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rms-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-antd-skeleton app is running!');
   });
 });
